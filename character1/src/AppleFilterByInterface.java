4ef116bc6126c5308bce4bf3b3d2cbfd56084849
zjj <zjjjava@foxmail.com>
2021-11-14 22:25:43 +0800

    无变化

--- character1/src/AppleFilterByInterface.java
+++ character1/src/AppleFilterByInterface.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 
@@ -71,5 +72,8 @@ public class AppleFilterByInterface<T> {
         System.out.println(inventory);
 
 
+
+
     }
+
 }
