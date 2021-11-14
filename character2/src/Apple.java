1fafdd523e13aa2b0117789888b473f7cb9a8e1d
zjj <zjjjava@foxmail.com>
2021-11-14 22:08:57 +0800

    方法引用

---
+++ character2/src/Apple.java
@@ -0,0 +1,44 @@
+public class Apple {
+    private String color;
+    private Integer weight;
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Apple(String color, Integer weight) {
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public Apple() {
+    }
+
+    public Apple(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Apple(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Apple{" +
+                "color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
--- character2/src/ReferenceLambda.java
+++ character2/src/ReferenceLambda.java
@@ -1,4 +1,8 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiPredicate;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class ReferenceLambda {
     public static void main(String[] args) {
@@ -12,6 +16,28 @@ public class ReferenceLambda {
         //先计算g再去计算f
         result=r.apply(12);
         System.out.println(result);
+
+        List<String> list= Arrays.asList("zjj","zjj","zjj","zjj","zjj","xjj");
+        BiPredicate<List<String>,String> contains=(inventory,element)->inventory.contains(element);
+//        contains代表的是BiPredicate的一个实体类的名字
+        System.out.println(contains.test(list, "hfdhfdsj"));
+        //改写为方放的引用为
+        BiPredicate<List<String>,String> containsReference=List::contains;
+        System.out.println(containsReference.test(list, "zjj"));
+
+        //方法的引用之构造函数
+        //创建对象使用Supplier函数式接口
+        Supplier<Apple> c1=()->new Apple("grenn",120);
+        System.out.println(c1);
+        //改造为方法引用
+        Supplier<Apple> c2=Apple::new;
+        // c2是Supplier的一个实体类的类名
+        System.out.println(c2.get());
+
+        //有参构造函数使用Function
+        Function<Integer,Apple> c3=Apple::new;
+        System.out.println(c3.apply(120));
+
 
     }
 
